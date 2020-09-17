aaf4b830058d4d2777088cd3ab943fdfa89f2151
SunnyBarua <sunnybarua95@gmail.com>
2020-09-17 14:22:23 +0600

    Updates

---
+++ precache-manifest.db3c35bed21532fb5f725e2fefd8d6d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6690a263d7269e2800d0f10f5110735",
+    "url": "/ema-john-eshop-MERN-/index.html"
+  },
+  {
+    "revision": "ce9b2e313a551de9dc1b",
+    "url": "/ema-john-eshop-MERN-/static/css/main.306e84d6.chunk.css"
+  },
+  {
+    "revision": "90f42cadf9642a9559ca",
+    "url": "/ema-john-eshop-MERN-/static/js/2.c25b35e4.chunk.js"
+  },
+  {
+    "revision": "df29ce23dc71344ae95e2113fe8322a8",
+    "url": "/ema-john-eshop-MERN-/static/js/2.c25b35e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce9b2e313a551de9dc1b",
+    "url": "/ema-john-eshop-MERN-/static/js/main.784cd018.chunk.js"
+  },
+  {
+    "revision": "e3c4fdb1831a2ec45df7",
+    "url": "/ema-john-eshop-MERN-/static/js/runtime-main.3436c849.js"
+  },
+  {
+    "revision": "95f238a51a560680815e09e999af5cb9",
+    "url": "/ema-john-eshop-MERN-/static/media/logo.95f238a5.png"
+  }
+]);
